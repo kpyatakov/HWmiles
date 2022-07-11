2ff420b6e7b07a8a7f6c6f6f34077f2014888d05
КириллПятаков <kpyatakov@gmail.com>
2022-07-11 07:18:11 +0700

    Мили модернизрованы,добавлен новый классв котором находится логика рассчета

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int ticket = 600; //вводится стоимость билета
-        int value = 20; //количество рублей за милю
-        int bonus = ticket / value;
-
-        System.out.println(bonus + " бонусных(ая) миль(и) начисленны(а)");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
